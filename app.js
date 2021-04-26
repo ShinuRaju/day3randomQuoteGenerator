63e04298f391a2e8cc1b700a9cfe6c434e09477a
Shinu <rajushinu007@gmail.com>
2021-04-26 10:05:11 +0530

    Trigger rebuild

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-import {quoteArray} from  './quote.js';
+import { quoteArray } from "./quote.js";
 
+let quoteContainer = document.querySelector(".container .quote-container");
 
-let quoteContainer = document.querySelector('.container .quote-container');
+let p = document.querySelector(".quote-container p");
+let btn = Array.from(document.getElementsByTagName("button"));
 
-let p = document.querySelector('.quote-container p');
-let btn = Array.from(document.getElementsByTagName('button'));
-
-let randomNumber = (min, max) =>{
-   return Math.floor(Math.random() * (max - min) + min)
-}
+let randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
 p.textContent = quoteArray[randomNumber(0, 100)];
 
 btn.forEach((eachElement) => {
-    eachElement.addEventListener('click', () =>{
-        p.textContent = quoteArray[randomNumber(0, 100)];
-        quoteContainer.style.backgroundImage = `url(https://picsum.photos/500?random=${randomNumber(0, 100)})`; 
-        
-    })
-})
+  eachElement.addEventListener("click", () => {
+    p.textContent = quoteArray[randomNumber(0, 100)];
+    quoteContainer.style.backgroundImage = `url(https://picsum.photos/500?random=${randomNumber(
+      0,
+      100
+    )})`;
+  });
+});
